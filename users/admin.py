63cb2df9ab0f4c3d0e2d75844f83109723b65374
Slava Kulikov <slava_kulikov@outlook.com>
2025-05-02 21:04:33 +0300

    Create models 'User', 'Subscription'.

---
+++ users/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import Subscription, User
+
+admin.site.empty_value_display = 'Не указано'
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'email'
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    list_display_links = ('user', 'author')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
---
+++ users/models.py
@@ -0,0 +1,68 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from core.constants import EMAIL_MAX_LENGTH, NAME_MAX_LENGTH
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        max_length=EMAIL_MAX_LENGTH,
+        verbose_name='Адрес электронной почты'
+    )
+    username = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Имя пользователя'
+    )
+    first_name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Фамилия'
+    )
+    avatar = models.ImageField(
+        'Аватар',
+        upload_to='avatars/',
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'объект "Подписка"'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_user_author'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} follows {self.author}'
